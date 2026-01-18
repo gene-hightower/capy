e872b5d285ba748c194d79ffc9ce2b8e476f5bbe
Vinnie Falco <vinnie.falco@gmail.com>
2026-01-18 11:22:27 -0800

    bcrypt is in http

--- include/boost/capy.hpp
+++ include/boost/capy.hpp
@@ -14,7 +14,6 @@
 #include <boost/capy/application.hpp>
 #include <boost/capy/ex/async_op.hpp>
 #include <boost/capy/ex/run_async.hpp>
-#include <boost/capy/bcrypt.hpp>
 #include <boost/capy/brotli.hpp>
 #include <boost/capy/buffers/any_read_source.hpp>
 #include <boost/capy/buffers/any_sink.hpp>
