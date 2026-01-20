75ff6202d7a352317ab886c830d8719e3f874eba
Vinnie Falco <vinnie.falco@gmail.com>
2026-01-20 02:55:56 -0800

    Remove unused stream types

--- include/boost/capy.hpp
+++ include/boost/capy.hpp
@@ -15,14 +15,10 @@
 #include <boost/capy/application.hpp>
 #include <boost/capy/ex/run_async.hpp>
 #include <boost/capy/brotli.hpp>
-#include <boost/capy/buffers/any_read_source.hpp>
-#include <boost/capy/buffers/any_sink.hpp>
-#include <boost/capy/buffers/any_stream.hpp>
 #include <boost/capy/buffers.hpp>
 #include <boost/capy/buffers/buffer_pair.hpp>
 #include <boost/capy/buffers/circular_buffer.hpp>
 #include <boost/capy/buffers/copy.hpp>
-#include <boost/capy/buffers/data_source.hpp>
 #include <boost/capy/buffers/dynamic_buffer.hpp>
 #include <boost/capy/error.hpp>
 #include <boost/capy/buffers/flat_buffer.hpp>
@@ -33,7 +29,6 @@
 #include <boost/capy/buffers/slice.hpp>
 #include <boost/capy/buffers/string_buffer.hpp>
 #include <boost/capy/buffers/to_string.hpp>
-#include <boost/capy/concept/data_source.hpp>
 #include <boost/capy/concept/dynamic_buffer.hpp>
 #include <boost/capy/concept/executor.hpp>
 #include <boost/capy/concept/frame_allocator.hpp>
